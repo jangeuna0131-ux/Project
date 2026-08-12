ae98102baa88380fc39ee167e031095ac4d62c58
jangeuna0131-ux <jangeuna0131@gmail.com>
2026-08-12 10:05:38 +0900

    Add files via upload

--- 99_datebase/js/script.js
+++ 99_datebase/js/script.js
@@ -4,29 +4,35 @@ const scriptURL =
 const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function(e) {
 
     e.preventDefault();
+
+    msg.innerHTML = "전송 중...";
 
     fetch(scriptURL, {
         method: "POST",
         body: new FormData(form)
     })
 
-    .then((response) => {
+    .then(function(response) {
 
         msg.innerHTML = "Message sent successfully";
 
-        setTimeout(function () {
+        form.reset();
+
+        setTimeout(function() {
             msg.innerHTML = "";
         }, 5000);
 
-        form.reset();
-
     })
 
-    .catch((error) => {
-        console.error("Error!", error.message);
+    .catch(function(error) {
+
+        console.error("Error!", error);
+
+        msg.innerHTML = "전송 실패";
+
     });
 
 });

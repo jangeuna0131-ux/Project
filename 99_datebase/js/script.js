3e5140f19feb7b8c10302041a6fde9035109d06e
jangeuna0131-ux <jangeuna0131@gmail.com>
2026-08-12 09:40:00 +0900

    Add files via upload

--- 99_datebase/js/script.js
+++ 99_datebase/js/script.js
@@ -1,42 +1,20 @@
-<form name="submit-to-google-sheet">
-
-    <input
-        type="text"
-        name="name"
-        placeholder="이름"
-        required
-    >
-
-    <input
-        type="email"
-        name="email"
-        placeholder="이메일"
-        required
-    >
-
-    <button type="submit">
-        보내기
-    </button>
-
-    <span id="msg"></span>
-
-</form>
-
-<script>
 const scriptURL =
-"https://script.google.com/macros/s/AKfycbyLst13lr4QS8D0CbJPQx2VH9ksuago-UVddkwLsJDoh_3YpDoq4E8fKkBesqms-KuY5Q/exec";
+"https://script.google.com/macros/s/AKfycbyL1pHDWBmhZ2qTJP-uwDdI6nFMw5qdN_mBufgF5OmX/dev";
 
 const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
 
 form.addEventListener("submit", (e) => {
+
     e.preventDefault();
 
     fetch(scriptURL, {
         method: "POST",
         body: new FormData(form)
     })
+
     .then((response) => {
+
         msg.innerHTML = "Message sent successfully";
 
         setTimeout(function () {
@@ -44,9 +22,13 @@ form.addEventListener("submit", (e) => {
         }, 5000);
 
         form.reset();
+
     })
+
     .catch((error) => {
+
         console.error("Error!", error.message);
+
     });
+
 });
-</script>

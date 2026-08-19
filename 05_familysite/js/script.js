bebf90854bde0985b17e94e2ef9f21c3007c2bfa
jangeuna0131-ux <jangeuna0131@gmail.com>
2026-08-19 17:46:55 +0900

    Add files via upload

---
+++ 05_familysite/js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+
+
+    // Family Site 클릭
+    $(".family-site").click(function () {
+
+
+        // Family List 슬라이드 열기 / 닫기
+        $(".family-list").slideToggle();
+
+
+    });
+
+
+});
